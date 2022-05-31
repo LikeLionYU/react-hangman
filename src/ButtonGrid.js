8c2e80082a3bff296770f835218315dbb9b959ca
gimwise <sophia1142004@gmail.com>
2022-06-01 00:34:45 +0900

    alphabet

--- src/ButtonGrid.js
+++ src/ButtonGrid.js
@@ -5,7 +5,7 @@ const ButtonGrid = (guessedLetterHandler, isClickable) => {
 
     let letters = [
         'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
-        'I', 'G', 'K', 'L', 'M', 'N', 'O', 'P',
+        'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
         'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X',
         'Y', 'Z'
     ];
